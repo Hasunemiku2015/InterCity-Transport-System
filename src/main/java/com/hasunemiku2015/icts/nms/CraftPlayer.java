6c719149067f31f5f97d11d48d8f195f93d6c05b
Hasunemiku2015 <kwing889@gmail.com>
2022-06-18 14:55:55 +0800

    .

--- src/main/java/com/hasunemiku2015/icts/nms/CraftPlayer.java
+++ src/main/java/com/hasunemiku2015/icts/nms/CraftPlayer.java
@@ -2,7 +2,7 @@ package com.hasunemiku2015.icts.nms;
 
 import com.hasunemiku2015.refraction.annotations.BaseClass;
 
-@BaseClass(name = "org.bukkit.craftbukkit.%s.entity.CraftPlayer")
+@BaseClass(name = "org.bukkit.craftbukkit.${VERSION}.entity.CraftPlayer")
 public interface CraftPlayer {
     EntityPlayer getHandle();
 }
