ec94a039b977bc134f16037b1499988dca59cb87
Hasunemiku2015 <kwing889@gmail.com>
2022-07-18 11:56:03 +0800

    1.6-Updated, Changed to Kotlin and HTTP Request

--- src/main/java/com/hasunemiku2015/icts/nms/CraftPlayer.java
+++
@@ -1,8 +0,0 @@
-package com.hasunemiku2015.icts.nms;
-
-import com.hasunemiku2015.refraction.annotations.BaseClass;
-
-@BaseClass(name = "org.bukkit.craftbukkit.${VERSION}.entity.CraftPlayer")
-public interface CraftPlayer {
-    EntityPlayer getHandle();
-}
--- src/main/java/com/hasunemiku2015/icts/nms/PlayerConnection.java
+++
@@ -1,9 +0,0 @@
-package com.hasunemiku2015.icts.nms;
-
-import com.hasunemiku2015.refraction.annotations.Abstracted;
-import com.hasunemiku2015.refraction.annotations.BaseClass;
-
-@BaseClass(name = "net.minecraft.server.${VERSION}.PlayerConnection")
-public interface PlayerConnection {
-    void sendPacket(@Abstracted PacketPlayOutMount packet);
-}
